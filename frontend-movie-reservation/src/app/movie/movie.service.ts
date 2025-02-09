79975dc55d88d7d827a55ba0cefc9b700b973910
Sergio Villanueva <sergiovillanueva@protonmail.com>
2025-02-09 17:48:27 -0300

    frontend: Fix MovieService.getUpcomingMovies response

--- frontend-movie-reservation/src/app/movie/movie.service.ts
+++ frontend-movie-reservation/src/app/movie/movie.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
+import {PageMovieResponse} from "./page-movie-response";
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,7 @@ export class MovieService {
   constructor(private http: HttpClient) {
   }
 
-  getUpcomingMovies(page: number): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}?page=${page}`);
+  getUpcomingMovies(page: number): Observable<PageMovieResponse> {
+    return this.http.get<PageMovieResponse>(`${this.apiUrl}?page=${page}`);
   }
 }
